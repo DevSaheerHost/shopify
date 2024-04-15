9d374740c3f657e96b2d1df2bc200b35f220eafc
Saheer Babu <122529965+DevSaheerHost@users.noreply.github.com>
2024-04-15 20:37:19 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,71 @@
 
 
 
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCsTD5XSRNl7VG-i6Ir0F3D1X1PxWk2Rfs",
+  authDomain: "shopify-30670.firebaseapp.com",
+  databaseURL: "https://shopify-30670-default-rtdb.firebaseio.com",
+  projectId: "shopify-30670",
+  storageBucket: "shopify-30670.appspot.com",
+  messagingSenderId: "792157900529",
+  appId: "1:792157900529:web:32d02d2d8b3fe05d94e350",
+  measurementId: "G-MZC38NN5BZ"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+var database = firebase.database();
+var ref = firebase.database().ref('shopless/home/ads/');
+
+
+// Assuming you want to access an item with the key 'itemId'
+ref.child('product_image').once('value', (snapshot) => {
+  const item = snapshot.val();
+  console.log(item);
+
+  //document.querySelector(".addImgMobile").src=item
+    document.querySelector(".addImage").src=item
+});
+
+
+database.ref("shopless/home/ads/").on("child_added", (snapshot) => {
+  //console.log('-----getting data Started--------')
+  const data = snapshot.val();
+ // console.log('create function')
+  // create(data.title, data.image, data.subtitle, data.pageurl);
+  //console.log('-------getting Stopped------')
+  
+  // var name = data.product_name;
+  // var img = data.product_image;
+  // var price = data.product_price;
+  // var quantity = data.quantity;
+  
+      
+  console.log(data)
+
+  
+  // const img = document.querySelector("#img")
+  // const name = document.querySelector("#name")
+  // const price = document.querySelector("#price")
+  // const description = document.querySelector("#description")
+  // img.src=data.product_image
+  // name.textContent= data.product_name
+  // price.textContent= data.product_price
+ // description.textContent= data.product_description
+
+    
+
+
+  // <img src="${data.product_image}" alt="">
+  // <h1 id="name">${data.product_name}</h1>
+  // <h3 id="price">${data.product_price}</h3>
+  // <label id="description" for="">${data.product_description}</label>
+  // 
+  
+});
+// -------------------------------------------------//
+
 $(document).ready(function () {
   console.log('ready')
   document.title="Shopify";
